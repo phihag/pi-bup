c7aaf58ba800e3ac798f91f18c10349f2ef0c582
Philipp Hagemeister <phihag@phihag.de>
2017-10-27 03:32:00 +0200

    start without esesion restore

--- bup-autostart.sh
+++ bup-autostart.sh
@@ -8,4 +8,6 @@ xset s off # don't activate screensaver
 xset -dpms # disable DPMS (Energy Star) features.
 xset s noblank # don't blank the video device
 
-chromium-browser --start-fullscreen "http://login.badmintonticker.de/ticker/bup/#btde&display&court=1&dm_style=teamcourt"
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
+chromium-browser --start-fullscreen --fast "http://login.badmintonticker.de/ticker/bup/#btde&display&court=1&dm_style=teamcourt"
