e664e8a7eee03077981e833f5640d6e9f02aa7fa
Philipp Hagemeister <phihag@phihag.de>
2019-12-09 22:25:22 +0100

    add example for bts

--- bup-autostart.sh
+++ bup-autostart.sh
@@ -10,4 +10,9 @@ xset s noblank # don't blank the video device
 
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
-chromium-browser --start-fullscreen --fast "https://badmintonticker.de/ticker/bup/#btde&display&court=1&dm_style=teamcourt"
+
+# badmintonticker
+# -chromium-browser --start-fullscreen --fast "https://badmintonticker.de/ticker/bup/#btde&display&court=1&dm_style=teamcourt"
+
+# bts tournaments
+chromium-browser --start-fullscreen --fast "https://10.10.1.2:4000/d"
